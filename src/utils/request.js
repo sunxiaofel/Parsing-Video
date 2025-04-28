510bbec561f15d570d22acd8a5e05de883e47903
sunxiaofei <sunxiaofei1422@gmail.com>
2025-04-28 22:08:09 +0800

    fix：timeout

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // 这里直接写 import.meta.env.VITE_API_URL
-  timeout: 30000,
+  timeout: 300000,
 });
 
 export default instance;
